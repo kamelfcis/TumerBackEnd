7db99c6c2f48220bc61beba24df0349130ea8a2e
kamelfcis <kamelfcis@gmail.com>
2024-10-31 09:48:56 +0300

    Add project files.

---
+++ TumerBackEnd/EFModels/Connections.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TumerBackEnd.EFModels
+{
+    public partial class Connections
+    {
+        public Guid Id { get; set; }
+        public Guid? PersonId { get; set; }
+        public string SignalrId { get; set; }
+        public DateTime? TimeStamp { get; set; }
+    }
+}
---
+++ TumerBackEnd/EFModels/TumerContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TumerBackEnd.EFModels
+{
+    public partial class TumerContext : DbContext
+    {
+         
+
+        public TumerContext(DbContextOptions<TumerContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Connections> Connections { get; set; }
+        public virtual DbSet<Person> Person { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                 optionsBuilder.UseSqlServer("Data Source=DESKTOP-7Q8JPT0;Initial Catalog=Tumer; Trusted_Connection=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Connections>(entity =>
+            {
+                entity.ToTable("connections");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.PersonId).HasColumnName("personId");
+
+                entity.Property(e => e.SignalrId)
+                    .HasMaxLength(22)
+                    .HasColumnName("signalrId");
+
+                entity.Property(e => e.TimeStamp)
+                    .HasColumnType("datetime")
+                    .HasColumnName("timeStamp");
+            });
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.ToTable("person");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Username)
+                    .HasMaxLength(50)
+                    .HasColumnName("username");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
